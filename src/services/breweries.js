b9b9ec6730076d074958d6dbce9ebb85d59a7d1b
ajoy267 <andrew.joy12@hotmail.com>
2022-01-25 16:42:31 -0800

    scrapped the API i was using and am using the API from my old Pokemon compendium lab

--- src/services/breweries.js
+++
@@ -1,6 +0,0 @@
-export async function fetchBreweries() {
-  const resp = await fetch('https://api.openbrewerydb.org/breweries');
-  const data = await resp.json();
-
-  return data;
-}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,24 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import BreweryCard from '../../components/BreweryCard';
-import { fetchBreweries } from '../../services/breweries';
 import './Home.css';
 
 export default function Home() {
-  const [breweries, setBreweries] = useState({});
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const getBreweries = async () => {
-      const resp = await fetchBreweries();
-      setBreweries(resp);
-      setLoading(false);
-    };
-    getBreweries();
-  }, [loading]);
   if (loading) return <h1>loading...</h1>;
-  return (
-    <div>
-      <BreweryCard breweries={breweries} />
-    </div>
-  );
+
+  return <div>Pokemon</div>;
 }
